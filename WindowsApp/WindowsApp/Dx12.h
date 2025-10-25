f5d015b2db10a9628a76347bfa50393e315bca08
Iwaiushio <ktc25a31a0006@edu.kyoto-tech.ac.jp>
2025-10-25 18:05:22 +0900

    DX12の初期化

---
+++ WindowsApp/WindowsApp/Dx12.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <d3d12.h>
+#include <dxgi1_4.h>
+class Dx12
+{
+public:
+	Dx12(){}
+	~Dx12(){}
+
+	IDXGIFactory4* CreateDXGIFactory();
+	IDXGIAdapter* GetHardwareAdapter(IDXGIFactory4* factory);
+	ID3D12Device* CreateD3D12Device(IDXGIAdapter1* adapter);
+	ID3D12CommandQueue* CreateCommandQueue(ID3D12Device* device);
+	IDXGISwapChain3* CreateSwapChain(IDXGIFactory4* factory, ID3D12CommandQueue* commandQueue, HWND hwnd);
+	void EnableDebugLayer();
+};
+
