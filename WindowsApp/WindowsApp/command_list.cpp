4f01a5a8e0266fd1db1419fc24f528ead296b250
minakuchi ryosuke <ktc23a31a0043@edu.kyoto-tech.ac.jp>
2025-12-04 00:48:23 +0900

    一応全部書けたはず

--- WindowsApp/WindowsApp/command_list.cpp
+++ WindowsApp/WindowsApp/command_list.cpp
@@ -1,1 +1,63 @@
-#include"CommandAllocator.h"
+//コマンドアロケータ制御クラス
+#include"command_list.h"
+#include"cassert"
+
+//デストラクタ
+CommandAllocator::~CommandAllocator() {
+	//コマンドアロケータの解放
+	if (commandAllocator_) {
+		commandAllocator_->Release();
+		commandAllocator_ = nullptr;
+	}
+}
+//---------------------------------------------------------------------------------
+/**
+ * @brief	コマンドキューの生成
+ * @param	device	デバイスクラスのインスタンス
+ * @return	成功すれば true
+ */
+[[nodiscard]] bool CommandAllocator::create(const Dx12& dx12, const D3D12_COMMAND_LIST_TYPE type)noexcept {
+	//コマンドリストのタイプを設定
+	type_ = type;
+	//コマンドアロケータの生成
+	const auto hr = dx12.device->CreateCommandAllocator(type_, IID_PPV_ARGS(&commandAllocator_));
+	if (FAILED(hr)) {
+		assert(false && "コマンドアロケータの作成に失敗しました");
+		return false;
+	}
+
+	return true;
+}
+//---------------------------------------------------------------------------------
+/**
+ * @brief	コマンドアロケータをリセットする
+ */
+void CommandAllocator::reset()noexcept {
+	if (!commandAllocator_) {
+		assert(false && "コマンドアロケータが未作成です");
+	}
+	commandAllocator_->Reset();
+}
+//---------------------------------------------------------------------------------
+/**
+ * @brief	コマンドアロケータを取得する
+ * @return	コマンドアロケータのポインタ
+ */
+[[nodiscard]] ID3D12CommandAllocator* CommandAllocator::get() const noexcept {
+	if (!commandAllocator_) {
+		assert(false && "コマンドアロケータが未作成です");
+		return nullptr;
+	}
+	return commandAllocator_;
+}
+//---------------------------------------------------------------------------------
+/**
+ * @brief	コマンドリストのタイプを取得する
+ * @return	コマンドリストのタイプ
+ */
+[[nodiscard]] D3D12_COMMAND_LIST_TYPE CommandAllocator::getType()const noexcept {
+	if (!commandAllocator_) {
+		assert(false && "コマンドリストのタイプが未設定です");
+	}
+	return type_;
+}
--- WindowsApp/WindowsApp/command_list.h
+++ WindowsApp/WindowsApp/command_list.h
@@ -1,2 +1,39 @@
 #pragma once
 #include"Dx12.h"
+#include"CommandAllocator.h"
+
+//---------------------------------------------------------------------------------
+/**
+ * @brief	コマンドリスト制御クラス
+ */
+class CommandList final {
+public:
+	//コンストラクタ
+	CommandList() = default;
+	//デストラクタ
+	~CommandList();
+
+	//---------------------------------------------------------------------------------
+	/**
+	 * @brief	コマンドリスト作成
+	 * @param	device	デバイスクラスのインスタンス
+	 * @param	command	コマンドアロケータクラスのインスタンス
+	 * @return	生成の成否
+	 */
+	[[nodiscard]] bool create(const Dx12& dx12, CommandAllocator& commandAllocator)noexcept;
+	//---------------------------------------------------------------------------------
+	/**
+	* @brief	コマンドリストのリセット
+	* @param	commandAllocator	コマンドアロケータクラスのインスタンス
+	*/
+	void reset(const CommandAllocator& commandAllocator)noexcept;
+	//---------------------------------------------------------------------------------
+	/**
+	* @brief	コマンドリストを取得する
+	* @return	コマンドリストのポインタ
+	*/
+	[[nodiscard]] ID3D12GraphicsCommandList* get()const noexcept;
+
+private:
+	ID3D12GraphicsCommandList* commandList_{};		//コマンドリスト
+};
