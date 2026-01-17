12aa2ccff84e85b32454855462cc6b03b9bbd9d7
MinakuchiRyosuke <ktc23a31a0043@edu.kyoto-tech.ac.jp>
2026-01-17 18:07:21 +0900

    input.hを書いた

--- WindowsApp/WindowsApp/input.h
+++ WindowsApp/WindowsApp/input.h
@@ -21,9 +21,29 @@ public:
 
 	//キー情報の取得
 	//key  キーの識別子
+	//return  入力したらtrue
+	[[nodiscard]] bool getTrigger(uint16_t skey) const noexcept;
+
+	//キー情報の更新
+	//pState  ハードから設定されたキー情報配列のポインタ
+	void updateKeyState(void* pState) noexcept;
+
+	//キー情報の更新
+	void updatePrevKeyState() noexcept;
 
 private:
+	//シングルトンパターンにするため、コンストラクタとデストラクタをprivateにする
 	Input() = default;
-	~Input();
+	~Input() = default;
+
+	//コピーとムーブの禁止
+	Input(const Input& r) = delete;
+	Input& operator= (const Input& r) = delete;
+	Input(Input&& r)  = delete;
+	Input& operator =(Input&& r) = delete;
+
+private:
+	std::array<byte, 256> keyState_{};		//ハードから設定されたキー情報配列
+	std::array<byte, 256> prevKeyState_{};	//ハードから設定されたキー情報配列
 };
 
